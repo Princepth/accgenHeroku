f2a99d2822ffca6589bf144691164c2797e03aea
Princepth <101026965+Princepth@users.noreply.github.com>
2022-03-24 14:47:42 -0400

    Update new.py

--- new.py
+++ new.py
@@ -1,6 +1,6 @@
 key='742bab95-f5ff-4d50-ab12-ee8934eeca5b'
 app_name="accheroku"
 deviceid=""
-custompwd=""
-url =""
-nickname= "itsme"
+custompwd="92734803"
+url ="https://replitgen-36.reginamarques.repl.co/"
+nickname= "Prc₹"
