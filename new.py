77a0444867173bfe285f78aaee5fcba0a0a163a5
Princepth <101026965+Princepth@users.noreply.github.com>
2022-03-24 14:48:51 -0400

    Update new.py

--- new.py
+++ new.py
@@ -1,6 +1,6 @@
 key='742bab95-f5ff-4d50-ab12-ee8934eeca5b'
 app_name="accheroku"
-deviceid=""
+deviceid="4248EA6A1C58E77BE89D8B382F788DE4E74975ECE2BE959958AC51432B415F6AA6CD48371482412091"
 custompwd="92734803"
 url ="https://replitgen-36.reginamarques.repl.co/"
 nickname= "Prc₹"
